d1ce50b75a354abaa28b4b04b406948e66e90403
kkamangcode <87938309+kkamangcode@users.noreply.github.com>
2026-04-25 10:43:41 +0900

    Refactor place management in Streamlit app

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import streamlit as st
 
-places=[
+if "places" not in st.session_state:
+    st.session_state.places=[
     {"이름":"강원 메이커스페이스","지역":"춘천","대학교 내 여부":"X","예약 가능 여부":"O","예약 사이트":"stbc.or.kr","전화번호":"033-245-6560"},
     {"이름":"G-MAKER LAB","지역":"양양","대학교 내 여부":"O","예약 가능 여부":"O","예약 사이트":"전화상담만 가능","전화번호":"033-660-8262,033-660-8266"},
     {"이름":"KNU 메이커스페이스","지역":"춘천","대학교 내 여부":"O","예약 가능 여부":"O","예약 사이트":"knumakerspace.com","전화번호":"033-250-7314"},
     {"이름":"강릉제작소","지역":"강릉","대학교 내 여부":"X","예약 가능 여부":"O","예약 사이트":"www.gnmakerspace.com","전화번호":"033-650-3362"},
     {"이름":"JOY&DIY 메이커스페이스","지역":"원주","대학교 내 여부":"O","예약 가능 여부":"O","예약 사이트":"https://maker.halla.ac.kr/main/index.php","전화번호":"033-760-1364"}
 ]
+def add_place(places):
+    name = st.text_input("이름")
+    region = st.text_input("지역")
+    univer = st.radio("대학교 내 여부", ["O","X"])
+    reserve = st.radio("예약 가능 여부", ["O","X"])
+    site = st.text_input("예약 사이트")
+    phone = st.text_input("전화번호")
+
+    if st.button("장소 추가"):
+        new_place = {
+            "이름": name,
+            "지역": region,
+            "대학교 내 여부": univer,
+            "예약 가능 여부": reserve,
+            "예약 사이트": site,
+            "전화번호": phone
+        }
+
+        st.session_state.places.append(new_place)
+        st.write("장소가 추가되었습니다.")
+    else:
+        st.write("정보를 입력해주세요.")
 def show_all(places):
     st.subheader("전체 장소 보기")
     for place in places:
@@ -17,9 +40,9 @@ def show_all(places):
         st.write("예약 사이트는",place["예약 사이트"],"입니다")
         st.write("전화번호는",place["전화번호"],"입니다")
         st.write("---")
-def get_recommendations(data, region, reserve):
+def get_recommendations(places, region, reserve):
     result = []
-    for place in data:
+    for place in places:
         if place["지역"] == region and place["예약 가능 여부"] == reserve:
             result.append(place)
     return result
@@ -27,13 +50,15 @@ def get_recommendations(data, region, reserve):
 st.title("강원 청소년 생활 도우미")
 selected_region = st.selectbox("지역을 선택하세요", ["강릉","춘천","양양","원주"])
 selected_reserve = st.radio("예약 가능 여부를 선택하세요", ["O", "X"])
+add_place(st.session_state.places)
 if st.button("전체 보기"):
-    show_all(places)
+    show_all(st.session_state.places)
 if st.button("추천 보기"):
-    recommendations = get_recommendations(places, selected_region, selected_reserve)
+    recommendations = get_recommendations(st.session_state.places, selected_region, selected_reserve)
 
     if len(recommendations) == 0:
         st.write("조건에 맞는 장소가 없습니다")
+        st.write("장소를 추가하고 싶으시면 **장소 추가** 버튼을 눌러 주세요.")
     else:
         for place in recommendations:
             st.write("추천장소는",place["이름"],"입니다")
